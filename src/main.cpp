2e52b23572c0b330f6665138b217f058f5747652
R3tr0 <nikk3695@gmail.com>
2023-03-09 20:05:54 +0300

    Small demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,45 @@
 #include <cmath>
+#include <unistd.h>
 #include <iostream>
 #include "Screen.hpp"
 
-#define WIDTH	60
-#define HEIGHT	60
+#define WIDTH		80
+#define HEIGHT		40
+#define RADIUS		10
+#define RATIO		0.6
+#define SCREEN_W	110
 
 int main()
 {
 	Screen screen(WIDTH, HEIGHT);
 	screen.Clear();
+	screen.Fill('@');
 
-	for (uint32_t x = 1; x <= 30; x++)
-		screen.GetTile(x, x)->SetChar('#');
-	// screen.Fill('#');
-	// screen[630]->SetChar(' ');
-	// screen.SetOrigin(WIDTH / 2, HEIGHT / 2);
+	for (uint32_t x = 0; x <= 10; x++)
+	{
+		screen.GetTile(x + 40, std::sqrt(RADIUS * RADIUS - x * x) * RATIO + 10)->SetChar(' ');
+		screen.GetTile(x + 40, -std::sqrt(RADIUS * RADIUS - x * x) * RATIO + 10)->SetChar(' ');
+		screen.GetTile(-x + 40, std::sqrt(RADIUS * RADIUS - x * x) * RATIO + 10)->SetChar(' ');
+		screen.GetTile(-x + 40, -std::sqrt(RADIUS * RADIUS - x * x) * RATIO + 10)->SetChar(' ');
+	}
+	
+	while (true)
+	{
+		for (uint32_t x = 0; x <= SCREEN_W - WIDTH; x++)
+		{
+			screen.SetOrigin(x, 0);
+			usleep(100000 / 30.f);
+		}
+
+		usleep(100000);
+
+		for (uint32_t x = SCREEN_W - WIDTH; x >= 1; x--)
+		{
+			screen.SetOrigin(x, 0);
+			usleep(100000 / 30.f);
+		}
+	}
+
 	std::cin.get();
 	return 0;
 }
